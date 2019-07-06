9beae0eb377868442a6027a9d9399dc8ebbea266
CreativePenguin <panda501wp@gmail.com>
2019-07-06 19:22:33 -0400

    Adjust to use curl, and add commands to build from source

--- osx.sh
+++ osx.sh
@@ -1,26 +1,8 @@
 #!/bin/sh
 
+. ./unixutil.lib
+
 echo "Installing everything needed for 2019 FRC development for Macbook"
-
-FRC_YEAR="2019"
-
-VSCODE_WGET_LINK="https://go.microsoft.com/fwlink/?LinkID=620882"
-VSCODE_WGET_ZIP_FILE="index.html?LinkID=620882"
-VSCODE_INSTALL_PATH="$HOME/Applications"
-
-WPILIB_WGET_LINK="https://github.com/wpilibsuite/allwpilib/releases/download/v2019.4.1/WPILib_Mac-2019.4.1.tar.gz"
-WPILIB_WGET_ZIP_FILE="WPILib_Mac-2019.4.1.tar.gz"
-WPILIB_INSTALL_PATH="$HOME/frc$FRC_YEAR"
-
-NAVX_WGET_LINK="http://www.kauailabs.com/public_files/navx-mxp/navx-mxp-libs.zip"
-NAVX_WGET_ZIP_FILE="navx-mxp-libs.zip"
-NAVX_INSTALL_PATH="$WPILIB_INSTALL_PATH/NavX"
-
-CTRE_WGET_LINK="http://www.ctr-electronics.com//downloads/lib/CTRE_FRCLibs_NON-WINDOWS_v4.4.1.10.zip"
-CTRE_WGET_ZIP_FILE="CTRE_FRCLibs_NON-WINDOWS_v4.4.1.10.zip"
-CTRE_INSTALL_PATH="$WPILIB_INSTALL_PATH/CTRE"
-
-JAVA_HOME="$HOME/frc$FRC_YEAR/jdk"
 
 # Install Homebrew (package manager for macs)
 echo "=================================================
