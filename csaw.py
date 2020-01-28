e68f97298b68d7bc4ff1e7a189c432c5d7b83a8c
CreativePenguin <panda501wp@gmail.com>
2020-01-28 01:46:22 -0500

    Documentation template

--- csaw.py
+++ csaw.py
@@ -19,24 +19,34 @@ Usage: csaw <command> [candidate] [version]
         install     or i    <candidate> [version]
         uninstall   or rm   <candidate> <version>
         list        or ls   [candidate]
+        help        or h
 '''
-
-if NUM_INPUTS == 0:
-    print(NO_INPUTS_MSG)
-
-if NUM_INPUTS == 1:
-    command = sys.argv[1]
-    if input == "i" or input == 'install':
-        install(argv[2])
-
 def getOS():
-    """
+    """Returns current os that matches toml description
     sys.platform docs: https://docs.python.org/3/library/sys.html#sys.platform
-    toml is organized by OS. This function is used to detect current OS, to
-    know which one should be used by toml.
+    toml is organized by OS. This function is used to detect current OS, &
+    translates it so it can be read by toml
     """
-    platform = {"win32":"Windows", "darwin":"macOS", "linux":"Linux"}
-    return platform[sys.platform]
+    try:
+        platform = {"win32":"Windows", "darwin":"macOS", "linux":"Linux"}
+        return platform[sys.platform]
+    except KeyError:
+        if sys.platform.startswith("linux"):
+            return "Linux"
+        print("OS Invalid. csaw only works on windows, macOS, & linux")
+        raise OSError
 
 def install(argv2):
-    downloadInfo = toml.load("{}.toml".format(argv2))
+    downloadInfo = toml.load("{}.toml".format(argv2))["Windows"]
+
+# https://stackoverflow.com/questions/419163/what-does-if-name-main-do
+if __name__ == '__main__':
+    if NUM_INPUTS == 0:
+        print(NO_INPUTS_MSG)
+
+    if NUM_INPUTS == 1:
+        command = sys.argv[1]
+        if input == 'i' or input == 'install':
+            install(argv[2])
+        if input == 'help' or input == 'h':
+            print(HELP_MSG)
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. csaw documentation master file, created by
+   sphinx-quickstart on Sun Jan 26 12:46:04 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to csaw's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+.. automodule:: csaw
+    :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
